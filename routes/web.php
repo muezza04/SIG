3c1ccf6b8680353aaebb053122c9b8b83620ac98
muezza04 <fatryarja@gmail.com>
2025-01-03 23:44:07 +0700

    pratikum 8 gempa bumi

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/beranda', function () {
+//     return view('beranda');
+// });
+
+Route::get('/gempa', function () {
+    return view('gempa');
+});
+
+// Route::get('/peta',[PetaController::class, 'index'])->name('peta');
